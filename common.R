0eb15387269778e110e2ec6a150b1bf1c56f4d02
McKenna Farmer <51832180+mckfarm@users.noreply.github.com>
2024-07-11 20:32:16 -0500

    contig and single assembly analysis

---
+++ common.R
@@ -0,0 +1,5 @@
+phases_clean <- data.frame(phase = c("I", "II", "III", "IV", "V"),
+                           day = c(0, 237, 484, 512, 750))
+
+metagenome_days <- data.frame(sample = c("CAN1", "CAN2", "CAN_1", "CAN_2", "CAN_3", "CAN3"),
+                              day =c(451, 521, 549, 612, 671, 728))
